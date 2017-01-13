90848b8ec3d48c0bc8ad6759bb224ba1f4a38f65
Erica Wright <ericaw21@gmail.com>
2017-01-13 00:43:58 -0800

    add radio button for favorite drinks

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     var nicknameInput = $("input#nickname").val();
     var ageInput = $("input#age").val();
     var animals = $("#animals").val();
+    var drink = $("input:radio[name=drink]:checked").val();
 
     $(".name").text(nameInput);
     $(".nickname").text(nicknameInput);
